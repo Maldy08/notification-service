ae078c1a68dc908a947c5b6d633012c27ec45c03
CARLOS MALDONADO <karloz_0986@hotmail.com>
2023-10-26 13:56:47 -0700

    nada

--- functions/index.js
+++ functions/index.js
@@ -7,25 +7,59 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
+
 const { setGlobalOptions } = require("firebase-functions/v2");
-const { onDocumentUpdated } = require("firebase-functions/v2/firestore");
+const { onDocumentUpdated,onDocumentCreated} = require("firebase-functions/v2/firestore");
 const logger = require("firebase-functions/logger");
 setGlobalOptions({maxInstances:10});
 
 const {initializeApp} = require("firebase-admin/app");
-const {getFirestore} = require("firebase-admin/firestore");
+const { getFirestore } = require("firebase-admin/firestore");
 
 initializeApp();
+const db = getFirestore(); 
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-exports.productos = onRequest(async(req, res) => {
-    const result = await getFirestore()
-      .collection("productos")
-      .listDocuments()
-      res.json({result: result.productos});
+// exports.sendpushnotification = onDocumentUpdated("promociones/{id}", (event) => {
+//   const newValues = event.data.after.data();
+//   const prevValues = event.data.before.data();
+//   logger.log(prevValues);
+//   logger.log(newValues);
+// });
+
+exports.oncreateproducto = onDocumentCreated("productos/{id}", async (event) => {
+  const snapshot = event.data;
+  const data = snapshot.data();
+  const idnegocio = data.id_negocio;
+  const users = db.collection("users");
+  const q1 = users.where("email","==","camv29@gmail.com");
+  const querySnapshot = await q1.get();
+  if(querySnapshot.size) {
+    querySnapshot.forEach( (doc) => {
+      doc.data().favorites_negocios.forEach( (f) => {
+        
+        if(idnegocio == f.id_negocio ){
+           logger.log(doc.data())
+
+           //mandar notificacionpush
+        }
+        else{
+          logger.log("nada")
+        }
+      });
+    });
+
+  }
+ // logger.log(querySnapshot.size);
+  // db.collection("users").get()
+  //   .then( (snapshot) => {
+  //     snapshot.forEach( (doc) => {
+  //       logger.log(doc.data().email);
+  //     })
+  //   })
+  
 });
 
 // exports.sendPushNotification = onDocumentUpdated("promociones/{id}", (event) => {
